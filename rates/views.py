a96597b3ba75255e1555091cb9500fed41e34272
Joao Paulo <joao.pereira@inatel.br>
2017-10-06 20:32:16 -0300

    Somando valor do IPCA

--- rates/views.py
+++ rates/views.py
@@ -1,7 +1,9 @@
 #coding: utf-8
 from datetime import datetime
 import json
+
 from django.http import HttpResponse
+import django.db.models
 
 from rates.models import Measure
 # Create your views here.
@@ -10,16 +12,22 @@ from rates.models import Measure
 def get_rate(request):
     date_pattern = "%d-%m-%Y"
     try:
-        import ipdb;ipdb.set_trace()
-        # filters = {}
         raw_filters = request.GET.get('filters', None)
+        function = request.GET.get('function')
         filters = json.loads(raw_filters.replace("'", '"'))
         for key in filters.keys():
             if 'date' in key:
                 raw_value = filters[key]
                 filters[key] = datetime.strptime(raw_value, date_pattern)
-        print(filters)
-        measure = Measure.objects.filter(**filters)[0]
-        return HttpResponse(measure.measure)
+        queryset = Measure.objects.filter(**filters)
+        if(function):
+            aggregate = getattr(django.db.models, function)('measure')
+            value = queryset.aggregate(aggregate)
+            key = "measure__" + function.lower()
+            value = value.get(key,None)
+        else:
+            value = queryset[0].measure
+
+        return HttpResponse(value)
     except IndexError:
         return HttpResponse('')
