04525630d187aa5812818329a0487b88132dcbbd
Joao Paulo <joao.pereira@inatel.br>
2017-11-28 14:13:27 -0200

    Adicionando campos na listagem de measure do django admin

--- rates/admin.py
+++ rates/admin.py
@@ -2,5 +2,9 @@ from django.contrib import admin
 from .models import Measure
 
 
+
+class MeasureAdmin(admin.ModelAdmin):
+    list_display = ('measure_date', 'rate', 'measure', )
+
 admin.site.register(Measure)
 # Register your models here.
