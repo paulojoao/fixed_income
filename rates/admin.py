1eebfd32452902f1659d7b078cdfc197ba1fe007
Joao Paulo <joao.pereira@inatel.br>
2019-02-20 19:28:15 -0300

    incluindo filtros

--- rates/admin.py
+++ rates/admin.py
@@ -5,6 +5,7 @@ from .models import Measure
 
 class MeasureAdmin(admin.ModelAdmin):
     list_display = ('measure_date', 'rate', 'measure', )
+    list_filter = ('rate', 'measure_date',)
 
 admin.site.register(Measure, MeasureAdmin)
 # Register your models here.
